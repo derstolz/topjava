42a727e4d8d052fa7e225139f3d80446e5036e58
Daniil <downtownxore@gmail.com>
2018-03-20 11:49:51 +0300

    apply patch with meals list on jsp

--- src/main/java/ru/javawebinar/topjava/model/MealWithExceed.java
+++ src/main/java/ru/javawebinar/topjava/model/MealWithExceed.java
@@ -25,6 +25,22 @@ public class MealWithExceed {
         this.exceed = isExceed;
     }
 
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public boolean isExceed() {
+        return exceed;
+    }
+
     @Override
     public String toString() {
         return "MealWithExceed{" +
