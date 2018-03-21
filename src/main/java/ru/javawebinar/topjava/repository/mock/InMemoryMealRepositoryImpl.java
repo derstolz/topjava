9b672baa14bb0a1b11c373ee2a18fb84c17247b3
Daniil <downtownxore@gmail.com>
2018-03-21 14:45:42 +0300

    meal rest controller

--- src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryMealRepositoryImpl.java
@@ -1,5 +1,7 @@
 package ru.javawebinar.topjava.repository.mock;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
@@ -12,6 +14,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 @Repository
 public class InMemoryMealRepositoryImpl implements MealRepository {
+
+    protected final Logger log = LoggerFactory.getLogger(getClass());
     private Map<Integer, Meal> repository = new ConcurrentHashMap<>();
     private AtomicInteger counter = new AtomicInteger(0);
 
@@ -21,6 +25,7 @@ public class InMemoryMealRepositoryImpl implements MealRepository {
 
     @Override
     public Meal save(Meal meal) {
+        log.info("save meal {}", meal);
         if (meal.isNew()) {
             meal.setId(counter.incrementAndGet());
             repository.put(meal.getId(), meal);
@@ -32,16 +37,19 @@ public class InMemoryMealRepositoryImpl implements MealRepository {
 
     @Override
     public boolean delete(int id) {
+        log.info("delete meal {}", id);
         return repository.remove(id, repository.get(id));
     }
 
     @Override
     public Meal get(int id) {
+        log.info("get meal {}", id);
         return repository.get(id);
     }
 
     @Override
     public Collection<Meal> getAll() {
+        log.info("get all meals");
         return repository.values();
     }
 }
--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -2,15 +2,37 @@ package ru.javawebinar.topjava.web.meal;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
+
+import java.util.List;
 
 @Controller
 public class MealRestController {
-
     private final MealService service;
 
     @Autowired
     public MealRestController(MealService service) {
         this.service = service;
+    }
+
+    public Meal create(Meal meal) {
+        return service.create(meal);
+    }
+
+    public Meal get(int id) {
+        return service.get(id);
+    }
+
+    public void update(Meal meal) {
+        service.update(meal);
+    }
+
+    public void delete(int id) {
+        service.delete(id);
+    }
+
+    public List<Meal> getAll() {
+        return service.getAll();
     }
 }
