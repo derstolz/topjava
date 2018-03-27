05d51112bb5b8ab791c663f573a2f4af8ddc6c04
Daniil <downtownxore@gmail.com>
2018-03-27 20:00:31 +0300

    HW2 repository

--- src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryUserRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryUserRepositoryImpl.java
@@ -1,14 +1,12 @@
 package ru.javawebinar.topjava.repository.mock;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
-import ru.javawebinar.topjava.model.AbstractNamedEntity;
-import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
-import java.util.*;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
@@ -16,16 +14,11 @@ import java.util.stream.Collectors;
 @Repository
 public class InMemoryUserRepositoryImpl implements UserRepository {
 
-    private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepositoryImpl.class);
+    public static final int USER_ID = 1;
+    public static final int ADMIN_ID = 2;
+
     private Map<Integer, User> repository = new ConcurrentHashMap<>();
     private AtomicInteger counter = new AtomicInteger(0);
-
-
-    @Override
-    public boolean delete(int id) {
-        log.info("delete {}", id);
-        return repository.remove(id, repository.get(id));
-    }
 
     @Override
     public User save(User user) {
@@ -34,34 +27,31 @@ public class InMemoryUserRepositoryImpl implements UserRepository {
             repository.put(user.getId(), user);
             return user;
         }
-        // treat case: update, but absent in storage
         return repository.computeIfPresent(user.getId(), (id, oldUser) -> user);
     }
 
     @Override
+    public boolean delete(int id) {
+        return repository.remove(id) != null;
+    }
+
+    @Override
     public User get(int id) {
-        log.info("get {}", id);
         return repository.get(id);
     }
 
     @Override
     public List<User> getAll() {
-        log.info("getAll");
-        List<User> users = new ArrayList<>(repository.values());
-        users.sort(Comparator.comparing(AbstractNamedEntity::getName));
-        return users;
+        return repository.values().stream()
+                .sorted(Comparator.comparing(User::getName).thenComparing(User::getEmail))
+                .collect(Collectors.toList());
     }
 
     @Override
     public User getByEmail(String email) {
-        log.info("getByEmail {}", email);
-        Set<User> users = repository.values()
-                .stream()
-                .filter(item -> item.getEmail().equals(email))
-                .collect(Collectors.toSet());
-        if (users.iterator().hasNext())
-            return users.iterator().next();
-        else
-            return null;
+        return repository.values().stream()
+                .filter(u -> email.equals(u.getEmail()))
+                .findFirst()
+                .orElse(null);
     }
 }
--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -2,6 +2,7 @@ package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.javawebinar.topjava.AuthorizedUser;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.repository.mock.InMemoryMealRepositoryImpl;
@@ -20,29 +21,28 @@ import java.util.Objects;
 public class MealServlet extends HttpServlet {
     private static final Logger log = LoggerFactory.getLogger(MealServlet.class);
 
-        private MealRepository repository;
+    private MealRepository repository;
 
-            @Override
+    @Override
     public void init(ServletConfig config) throws ServletException {
-                super.init(config);
-                repository = new InMemoryMealRepositoryImpl();
-            }
+        super.init(config);
+        repository = new InMemoryMealRepositoryImpl();
+    }
 
-            @Override
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-                request.setCharacterEncoding("UTF-8");
-                String id = request.getParameter("id");
+        request.setCharacterEncoding("UTF-8");
+        String id = request.getParameter("id");
 
-                        Meal meal = new Meal(id.isEmpty() ? null : Integer.valueOf(id),
-                                LocalDateTime.parse(request.getParameter("dateTime")),
-                                request.getParameter("description"),
-                                Integer.parseInt(request.getParameter("calories")));
+        Meal meal = new Meal(id.isEmpty() ? null : Integer.valueOf(id),
+                LocalDateTime.parse(request.getParameter("dateTime")),
+                request.getParameter("description"),
+                Integer.parseInt(request.getParameter("calories")));
 
-                        log.info(meal.isNew() ? "Create {}" : "Update {}", meal);
-                repository.save(meal);
-                response.sendRedirect("meals");
-            }
-
+        log.info(meal.isNew() ? "Create {}" : "Update {}", meal);
+        repository.save(meal, AuthorizedUser.id());
+        response.sendRedirect("meals");
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -52,14 +52,14 @@ public class MealServlet extends HttpServlet {
             case "delete":
                 int id = getId(request);
                 log.info("Delete {}", id);
-                repository.delete(id);
+                repository.delete(id, AuthorizedUser.id());
                 response.sendRedirect("meals");
                 break;
             case "create":
             case "update":
                 final Meal meal = "create".equals(action) ?
                         new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000) :
-                        repository.get(getId(request));
+                        repository.get(getId(request), AuthorizedUser.id());
                 request.setAttribute("meal", meal);
                 request.getRequestDispatcher("/mealForm.jsp").forward(request, response);
                 break;
@@ -67,7 +67,7 @@ public class MealServlet extends HttpServlet {
             default:
                 log.info("getAll");
                 request.setAttribute("meals",
-                        MealsUtil.getWithExceeded(repository.getAll(), MealsUtil.DEFAULT_CALORIES_PER_DAY));
+                        MealsUtil.getWithExceeded(repository.getAll(AuthorizedUser.id()), MealsUtil.DEFAULT_CALORIES_PER_DAY));
                 request.getRequestDispatcher("/meals.jsp").forward(request, response);
                 break;
         }
