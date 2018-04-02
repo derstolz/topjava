eba13c79e53a3f8df04c4b22d64c483b23e88c0b
Daniil <downtownxore@gmail.com>
2018-04-02 18:13:54 +0300

    working on jdbcMeal

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,8 +1,11 @@
 DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
+DROP SEQUENCE IF EXISTS global_meals_seq;
 
 CREATE SEQUENCE global_seq START 100000;
+CREATE SEQUENCE global_meals_seq START 1;
 
 CREATE TABLE users
 (
@@ -15,6 +18,17 @@ CREATE TABLE users
   calories_per_day INTEGER DEFAULT 2000    NOT NULL
 );
 CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
+
+CREATE TABLE meals
+(
+  id               INTEGER PRIMARY KEY DEFAULT nextval('global_meals_seq'),
+  userId           INTEGER                 NOT NULL,
+  dateTime         TIMESTAMP               NOT NULL,
+  description      VARCHAR                 NOT NULL,
+  calories         INTEGER                 NOT NULL,
+  FOREIGN KEY (userId) REFERENCES users (id)
+);
+CREATE UNIQUE INDEX meals_unique_id_idx ON meals (id);
 
 CREATE TABLE user_roles
 (
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,6 +1,8 @@
 DELETE FROM user_roles;
+DELETE FROM meals;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
+ALtER SEQUENCE global_meals_seq RESTART WITH 1;
 
 INSERT INTO users (name, email, password) VALUES
   ('User', 'user@yandex.ru', 'password'),
@@ -9,3 +11,8 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+INSERT INTO meals (userId, dateTime, description, calories) VALUES
+  (100000, '2018-04-02 09:00:00', 'Завтрак', 500),
+  (100000, '2018-04-02 12:00:00', 'Обед', 500),
+  (100001, '2018-04-02 09:00:00', 'Завтрак', 600),
+  (100001, '2018-04-02 12:00:00', 'Обед', 500)
