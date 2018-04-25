0f00bc5fffb2481ccfda51361f227714273c876b
Daniil <downtownxore@gmail.com>
2018-04-25 17:22:03 +0300

    working on different spring profiles

--- src/test/java/ru/javawebinar/topjava/SpringMain.java
+++ src/test/java/ru/javawebinar/topjava/SpringMain.java
@@ -1,7 +1,10 @@
 package ru.javawebinar.topjava;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.to.MealWithExceed;
@@ -13,22 +16,31 @@ import java.time.LocalTime;
 import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Properties;
 
 public class SpringMain {
-    public static void main(String[] args) {
-        // java 7 Automatic resource management
-        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml","spring/mock.xml")) {
-            System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
-            AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
-            adminUserController.create(new User(null, "userName", "email", "password", Role.ROLE_ADMIN));
-            System.out.println();
+//    public static void main(String[] args) {
+//        // java 7 Automatic resource management
+//        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml","spring/mock.xml")) {
+//            System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
+//            AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
+//            adminUserController.create(new User(null, "userName", "email", "password", Role.ROLE_ADMIN));
+//            System.out.println();
+//
+//            MealRestController mealController = appCtx.getBean(MealRestController.class);
+//            List<MealWithExceed> filteredMealsWithExceeded =
+//                    mealController.getBetween(
+//                            LocalDate.of(2015, Month.MAY, 30), LocalTime.of(7, 0),
+//                            LocalDate.of(2015, Month.MAY, 31), LocalTime.of(11, 0));
+//            filteredMealsWithExceeded.forEach(System.out::println);
+//        }
+//    }
 
-            MealRestController mealController = appCtx.getBean(MealRestController.class);
-            List<MealWithExceed> filteredMealsWithExceeded =
-                    mealController.getBetween(
-                            LocalDate.of(2015, Month.MAY, 30), LocalTime.of(7, 0),
-                            LocalDate.of(2015, Month.MAY, 31), LocalTime.of(11, 0));
-            filteredMealsWithExceeded.forEach(System.out::println);
-        }
+    public static void main(String[] args) {
+        System.setProperty("spring.profiles.active", "postgres");
+        ConfigurableApplicationContext applicationContext = new ClassPathXmlApplicationContext
+                ("classpath:spring/spring-app.xml", "classpath:spring/spring-db.xml");
+
+        System.out.println(Arrays.toString(applicationContext.getBeanDefinitionNames()));
     }
 }
